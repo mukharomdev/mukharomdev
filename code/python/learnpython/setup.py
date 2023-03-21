b1a83345075b5f838a3058bb1b00cf5397b960f9
mukharomdev <mukharomdev@gmail.com>
2023-03-21 15:54:30 +0700

    fix:modified code/python/learnpython*

--- code/python/learnpython/setup.py
+++ code/python/learnpython/setup.py
@@ -1,10 +1,18 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import textwrap
+
 from setuptools import setup
 
-setup(
-    name='learnpython',
-    version='0.0.1',
-    install_requires=[
-        'requests',
-        'importlib-metadata; python_version >= "3.8"',
-    ],
-)
+if __name__ == "__main__":
+	
+	setup(
+	    name='learnpython',
+	    version='0.0.1',
+	    install_requires=[
+	        'requests',
+	        'importlib-metadata; python_version >= "3.8"',
+	    ],
+	)
